851acb737b1337fd3375d46ed48faef2f365e50d
Luis Arias <luigianargo@gmail.com>
2021-05-12 01:24:30 -0600

    Add VulkanPrimitives class with dummy methods

---
+++ src/vulkan_primitives.cpp
@@ -0,0 +1,19 @@
+#include "vulkan_primitives.h"
+
+VulkanPrimitives::VulkanPrimitives()
+{
+    printf("Creating Instance\n");
+
+}
+
+
+void VulkanPrimitives::Init()
+{
+    printf("Initializing Vulkan\n");
+}
+
+
+void VulkanPrimitives::Process()
+{
+    printf("Processing a Primitive\n");
+}
---
+++ src/vulkan_primitives.h
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+class VulkanPrimitives
+{
+public:
+
+    VulkanPrimitives();
+
+    void Init();
+
+    void Process();
+
+};
+
